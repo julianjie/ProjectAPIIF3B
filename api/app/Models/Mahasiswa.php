6f3bbf27659612b564677b387a4633b6cddfdf5f
julianjie <128372866+julianjie@users.noreply.github.com>
2024-09-12 15:15:13 +0700

    generate model for mahasiswa

---
+++ api/app/Models/Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory,HasUuids;
+
+    public function prodi(){
+        return $this->belongsTo(Prodi::class, 'prodi_id');
+    }
+
+    protected $fillable = [
+        'npm',
+        'nama',
+        'tanggal_lahir',
+        'tempat_lahir',
+        'alamat',
+        'prodi_id'
+    ];
+}
